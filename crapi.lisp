21b28ab2f7aed924abf7d3030a81f13a6fe074e8
Ryan Gannon <ryanmgannon@gmail.com>
2023-11-06 22:48:21 -0500

    spec version validation

--- crapi.lisp
+++ crapi.lisp
@@ -5,3 +5,33 @@
   (:local-nicknames (:json :com.inuoe.jzon)))
 (in-package :crapi)
 
+;; Spec files (either Swagger 2 or OpenAPI 3/3.1) are parsed and CLOS objects are created representing
+;; the API.  From this object, we dump the client to a .lisp file to be loaded normally.
+
+(defun load-spec-file (filename)
+  "Load a JSON spec from a file, and return it as a hash-map for further processing"
+  (load-spec (read-file-into-string filename)))
+
+(defun load-spec (in)
+  "load a JSON spec from a json string or stream, and return it as a hash-map for further processing"
+  (let ((spec (json:parse in)))
+    (when (validate-spec in)
+      (error "Invalid JSON spec"))))
+
+(defun validate-spec (spec)
+  (and
+   (validate-spec-version spec)
+   ;; ...
+   ))
+
+(defun validate-spec-version (spec)
+  (let ((swagger-version (gethash "swagger" spec))
+        (openapi-version (gethash "openapi" spec)))
+    (cond
+      (swagger-version (if (equal swagger-version "2.0")
+                           t
+                           (error "Invalid spec version: Swagger 2.0 or OpenAPI 3.* required")))
+      (openapi-version (if (string^= "3." openapi-version)
+                           t
+                           (error "Invalid spec version: Swagger 2.0 or OpenAPI 3.* required")))
+      (t (error "Could not find OpenAPI/Swagger specification version at toplevel")))))
